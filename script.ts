e1954728b70679aa29a4f66e5e10af8b50fc014c
mzs21 <snigdho2111@gmail.com>
2021-07-02 20:31:06 +0600

    New changes have been added

--- script.ts
+++ script.ts
@@ -2,7 +2,6 @@ let playAudio = document.querySelector('#play') as HTMLAudioElement;
 
 let buttonElement = document.querySelector('button') as HTMLButtonElement;
 
-
 let enableAudio = () =>
 {
     playAudio.autoplay = true;
@@ -28,3 +27,60 @@ buttonElement.addEventListener('click', () =>
         disableAudio();
     }
 })
+
+let movieName = document.querySelector('.movie-name') as HTMLParagraphElement;
+
+movieName.addEventListener('animationstart', () =>
+{
+    movieName.innerText = "Your Name";
+});
+
+let directorName = document.querySelector('.director-name') as HTMLParagraphElement;
+
+directorName.addEventListener('animationstart', () =>
+{
+    directorName.innerText = "- Directed by Makoto Shinkai";
+});
+
+
+let paraElements = document.querySelector('.description') as HTMLParagraphElement;
+
+let paraElementOne = document.createElement("p");
+let paraElementTwo = document.createElement("p");
+let paraElementThree = document.createElement("p");
+let paraElementFour = document.createElement("p");
+
+let anchorElement = document.createElement("a");
+
+anchorElement.innerText = "here"; 
+anchorElement.setAttribute("href", "https://en.wikipedia.org/wiki/Your_Name");
+anchorElement.setAttribute("target", "_blank");
+anchorElement.className = "description";
+
+paraElements.appendChild(paraElementOne);
+paraElements.appendChild(paraElementTwo);
+paraElements.appendChild(paraElementThree);
+paraElements.appendChild(paraElementFour);
+
+paraElementOne.addEventListener('animationstart', () =>
+{
+    paraElementOne.innerText = "Your Name is a 2016 Japanese animated";
+});
+
+paraElementTwo.addEventListener('animationstart', () =>
+{
+    paraElementTwo.innerText = "romantic fantasy film produced by";
+});
+
+paraElementThree.addEventListener('animationstart', () =>
+{
+    paraElementThree.innerText = "CoMix Wave Films & released by Toho.";
+});
+
+paraElementFour.addEventListener('animationstart', () =>
+{
+    paraElementFour.innerText = "For more info click ";
+
+    paraElementFour.appendChild(anchorElement);
+});
+
--- script/script.js
+++ script/script.js
@@ -19,3 +19,38 @@ buttonElement.addEventListener('click', function () {
         disableAudio();
     }
 });
+var movieName = document.querySelector('.movie-name');
+movieName.addEventListener('animationstart', function () {
+    movieName.innerText = "Your Name";
+});
+var directorName = document.querySelector('.director-name');
+directorName.addEventListener('animationstart', function () {
+    directorName.innerText = "- Directed by Makoto Shinkai";
+});
+var paraElements = document.querySelector('.description');
+var paraElementOne = document.createElement("p");
+var paraElementTwo = document.createElement("p");
+var paraElementThree = document.createElement("p");
+var paraElementFour = document.createElement("p");
+var anchorElement = document.createElement("a");
+anchorElement.innerText = "here";
+anchorElement.setAttribute("href", "https://en.wikipedia.org/wiki/Your_Name");
+anchorElement.setAttribute("target", "_blank");
+anchorElement.className = "description";
+paraElements.appendChild(paraElementOne);
+paraElements.appendChild(paraElementTwo);
+paraElements.appendChild(paraElementThree);
+paraElements.appendChild(paraElementFour);
+paraElementOne.addEventListener('animationstart', function () {
+    paraElementOne.innerText = "Your Name is a 2016 Japanese animated";
+});
+paraElementTwo.addEventListener('animationstart', function () {
+    paraElementTwo.innerText = "romantic fantasy film produced by";
+});
+paraElementThree.addEventListener('animationstart', function () {
+    paraElementThree.innerText = "CoMix Wave Films & released by Toho.";
+});
+paraElementFour.addEventListener('animationstart', function () {
+    paraElementFour.innerText = "For more info click ";
+    paraElementFour.appendChild(anchorElement);
+});
